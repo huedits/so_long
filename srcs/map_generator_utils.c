a5eb6f298bfa1c10248a44b921822cd19950ce40
Huedits <huedits@gmail.com>
2022-09-23 16:14:58 -0300

    .

--- srcs/map_generator_utils.c
+++ srcs/map_generator_utils.c
@@ -25,29 +25,29 @@ void	write_hwall(int fd, size_t size, int final)
 		write(fd, "\n", 1);
 }
 
-void	get_random_dir(int *new_dir)
+void	get_random_dir(int *new_dir1, int *new_dir2)
 {
 	int	random_dir;
 
 	random_dir = random() % 4;
 	if (random_dir == 0)
 	{
-		*new_dir[0] = -1;
-		*new_dir[1] = 0;
+		*new_dir1 = -1;
+		*new_dir2 = 0;
 	}
 	if (random_dir == 1)
 	{
-		*new_dir[0] = 1;
-		*new_dir[1] = 0;
+		*new_dir1 = 1;
+		*new_dir2 = 0;
 	}
 	if (random_dir == 2)
 	{
-		*new_dir[0] = 0;
-		*new_dir[1] = -1;
+		*new_dir1 = 0;
+		*new_dir2 = -1;
 	}
 	if (random_dir == 3)
 	{
-		*new_dir[0] = 0;
-		*new_dir[1] = 1;
+		*new_dir1 = 0;
+		*new_dir2 = 1;
 	}
 }
